782aa917b4a87004d36bc98883c2403ce0f60bd9
loadams1 <loadams1@umbc.edu>
2014-03-09 14:27:14 -0400

    Parallax Scrolling
    Added parallax scrolling to character controller

--- Assets/Scripts/AliverController.cs
+++ Assets/Scripts/AliverController.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class AliverController : MonoBehaviour {
 
 	public Animator anim;
+	private Transform background;
 
+	public float ParallaxFactor = 1f;
 	public float maxSpeed = 10f;
 
 	public bool grounded = true;
@@ -12,9 +14,12 @@ public class AliverController : MonoBehaviour {
 	public float jumpForce = 1000f;
 	public LayerMask whatIsGround;
 
+	private Vector3 lastPos;
+
 	// Use this for initialization
 	void Start () {
-
+		lastPos = transform.position;
+		background = transform.FindChild ("Background");
 	}
 	
 	// Update is called once per frame
@@ -35,6 +40,12 @@ public class AliverController : MonoBehaviour {
 
 		float move = Input.GetAxis ("Horizontal");
 		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+
+		float xDist = transform.position.x - lastPos.x;
+		float yDist = transform.position.y - lastPos.y;
+		
+		background.Translate(-ParallaxFactor * xDist, -ParallaxFactor * yDist, 0);
+		lastPos = transform.position;
 	}
 
 
