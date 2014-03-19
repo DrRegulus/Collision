e7c812fc6b34ef0f4ecd8002214658518f592ee5
loadams1 <loadams1@umbc.edu>
2014-03-19 18:22:38 -0400

    Updated Level Design

---
+++ Assets/Scripts/SteamVent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteamVent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		float hForce = 1000f;
+
+		if (col.transform.position.x <= transform.position.x)
+			hForce *= -1;
+
+		col.rigidbody.rigidbody2D.AddForce (new Vector2 (hForce, 500f));
+	}
+}
