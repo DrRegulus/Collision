a2545a1efe580793573c809f801c918044189cc1
loadams1 <loadams1@umbc.edu>
2014-03-26 17:58:25 -0400

    Added Checkpoint Lamps

---
+++ Assets/Scripts/SetCheckpoint.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetCheckpoint : MonoBehaviour {
+
+	public Animator anim;
+
+	private SetCheckpoint[] checkpoints;
+	private AliverController aliver;
+
+	// Use this for initialization
+	void Start () {
+		checkpoints = FindObjectsOfType<SetCheckpoint> ();
+		aliver = FindObjectOfType<AliverController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == "Player")
+		{
+			foreach(SetCheckpoint chk in checkpoints)
+			{
+				if(this != chk)
+					chk.anim.SetBool("Activated", false);
+			}
+
+			anim.SetBool("Activated", true);
+			aliver.CheckPointReached(this, null);
+		}
+	}
+}
