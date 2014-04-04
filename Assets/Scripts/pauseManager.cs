32963afe9830a86ff0b3a6ae6981d4b3788a6fdf
rdg7739 <rdg7739@hotmail.com>
2014-04-04 11:45:23 -0400

    simple pause
    pause by press "P"

---
+++ Assets/Scripts/pauseManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class pauseManager : MonoBehaviour {
+	public GUIText pauseText;
+	public bool pause = false;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("p") && pause){
+			pause = false;
+			Time.timeScale = 1;
+			pauseText.enabled = false;
+
+		}
+		else if(Input.GetKeyDown("p") && !pause){
+			pause = true;
+			Time.timeScale = 0;
+			pauseText.enabled = true;
+
+		}
+	}
+}
